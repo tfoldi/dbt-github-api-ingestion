ce7c24a89825b3cb3a6de0bfb2661c6ac96a7bbd
Földi Tamás <tfoldi@starschema.net>
2022-10-08 17:36:55 +0000

    Implement pagination in python model

--- models/ingestions/stg_commits.sql
+++ models/ingestions/stg_commits.sql
@@ -1,1 +1,2 @@
-select STARSNOW_REQUEST('https://api.github.com/repos/{{var('gh_repository')}}/commits', NULL):data as github_commits
+select STARSNOW_REQUEST('https://api.github.com/repos/' ||
+    '{{var('gh_repository')}}/commits', NULL):data as github_commits
